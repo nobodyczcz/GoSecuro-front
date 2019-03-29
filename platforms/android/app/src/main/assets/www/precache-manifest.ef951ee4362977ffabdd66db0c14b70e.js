75416e5960fec8701750e379d879e3ce5e4c1fa9
nobodyczcz <jianshen858@gmail.com>
2019-03-30 10:49:24 +1100

    Project setup

---
+++ platforms/android/app/src/main/assets/www/precache-manifest.ef951ee4362977ffabdd66db0c14b70e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f63d479c48024cf0a146",
+    "url": "./static/js/main.935128f6.chunk.js"
+  },
+  {
+    "revision": "dc3a64d19d4abd4e48e8",
+    "url": "./static/js/2.992c039d.chunk.js"
+  },
+  {
+    "revision": "f63d479c48024cf0a146",
+    "url": "./static/css/main.c86850ed.chunk.css"
+  },
+  {
+    "revision": "e390869adc90eb751bdc732bf0be533c",
+    "url": "./index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MainBar from './AppBar'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const styles = {
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+};
 
 class App extends Component {
+  state = {
+    left: false,
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
   render() {
+
+    const sideList = (
+      <div>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <MainBar toggleDrawer={this.toggleDrawer}>
+        </MainBar>
+
+        <div>
+          Main Content
+        </div>
+
+        <SwipeableDrawer
+          open={this.state.left}
+          onClose={this.toggleDrawer('left', false)}
+          onOpen={this.toggleDrawer('left', true)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('left', false)}
+            onKeyDown={this.toggleDrawer('left', false)}
           >
-            Learn React
-          </a>
-        </header>
+            {sideList}
+          </div>
+        </SwipeableDrawer>
       </div>
     );
   }
